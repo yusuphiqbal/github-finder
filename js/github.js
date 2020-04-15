1b26a687c275ceb501194e84cbcc26e8758e4b37
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-04-15 11:08:28 +0300

    Added url and url parameters to fetch 5 latest repos of a user being displayed

--- js/github.js
+++ js/github.js
@@ -2,11 +2,15 @@ class GitHub {
   constructor() {
     this.clientId = '853bedb93cbb209d6b79';
     this.cliendSecret = 'be801362adfc680cde41ca8016721342c206fbbc';
+    this.reposCount = 5;
+    this.reposSort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.clientId}&client_secret=${this.cliendSecret}`);
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.reposCount}&sort=${this.reposSort}&client_id=${this.clientId}&client_secret=${this.cliendSecret}`);
     const profile = await profileResponse.json();
-    return { profile }
+    const repos = await reposResponse.json();
+    return { profile, repos }
   }
 }
